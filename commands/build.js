8fd0767bcc4db44afa5469221bc8a3077687b973
siriusmart <siriusmart@tuta.io>
2025-08-23 16:08:46 +0100

    fixed loads of stuff

--- commands/build.js
+++ commands/build.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 const path = require("node:path");
 
-function build(projectPath) {
+async function build(projectPath) {
     if (!fs.existsSync(path.join(projectPath, "./src")))
         require("./init").init(projectPath);
 
     console.log("Building of project started.");
     let start = Date.now();
-    require("../lib/build").build();
+    await require("../lib/build").build();
     console.log(`Build completed in ${Date.now() - start}ms.`);
 }
 
--- commands/entry.js
+++ commands/entry.js
@@ -1,29 +1,38 @@
 #!/usr/bin/env node
 let args = process.argv.slice(2);
 
+let fs = require("fs");
+
 let { init } = require("./init");
 let { build } = require("./build");
 let { serve } = require("./serve");
 let { install } = require("./install");
 
-let projectPath = ".";
+(async () => {
+    let projectPath = ".";
 
-switch (args[0]) {
-    case undefined:
-        serve(projectPath);
-        break;
-    case "init":
-        init(projectPath);
-        break;
-    case "build":
-        build(projectPath);
-        break;
-    case "serve":
-        serve(projectPath);
-        break;
-    case "install":
-        install(projectPath, ...args.slice(1));
-        break;
-    default:
-        console.error(`Unknown command ${args[0]}.`);
-}
+    if (args[0] != "install" && !fs.existsSync("./meta/templates")) {
+        console.log("Pulling required templates");
+        await install(projectPath);
+    }
+
+    switch (args[0]) {
+        case undefined:
+            await serve(projectPath);
+            break;
+        case "init":
+            await init(projectPath);
+            break;
+        case "build":
+            await build(projectPath);
+            break;
+        case "serve":
+            await serve(projectPath);
+            break;
+        case "install":
+            await install(projectPath, ...args.slice(1));
+            break;
+        default:
+            console.error(`Unknown command ${args[0]}.`);
+    }
+})();
